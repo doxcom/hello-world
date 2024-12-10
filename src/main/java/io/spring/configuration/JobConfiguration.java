62c2fdef0e86a7344eb560d8ff1a8be0f4d3c8c7
doxcom <aldoperez1319@gmail.com>
2024-12-09 21:14:32 -0600

    spring batch hello world initial commit and config

---
+++ src/main/java/io/spring/configuration/JobConfiguration.java
@@ -0,0 +1,42 @@
+package io.spring.configuration;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@EnableBatchProcessing
+public class JobConfiguration {
+
+
+    @Bean
+    public Job job(JobRepository jobRepository, Step step) {
+
+        return new JobBuilder("step1", jobRepository)
+                .start(step)
+                .build();
+    }
+
+    @Bean
+    public Step step(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("step", jobRepository)
+                .tasklet((StepContribution contribution,ChunkContext chunkContext) ->{
+                    System.out.println("hello world");
+                    return RepeatStatus.FINISHED;
+                },transactionManager).build();
+    }
+
+   }
+
+
