5e0572979996cd90594f5ab779c655e3fa63aaaf
doxcom <aldoperez1319@gmail.com>
2024-12-12 19:12:58 -0600

    intento de batch job

--- src/main/java/io/spring/configuration/JobConfiguration.java
+++ src/main/java/io/spring/configuration/JobConfiguration.java
@@ -8,25 +8,32 @@ import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.core.step.tasklet.Tasklet;
+
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
 
 @Configuration
 @EnableBatchProcessing
+@EnableTransactionManagement
+@EnableAutoConfiguration
 public class JobConfiguration {
+
 
 
     @Bean
     public Job job(JobRepository jobRepository, Step step) {
-
         return new JobBuilder("step1", jobRepository)
                 .start(step)
                 .build();
     }
+
 
     @Bean
     public Step step(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
--- src/main/java/io/spring/hello_world/HelloWorldApplication.java
+++ src/main/java/io/spring/hello_world/HelloWorldApplication.java
@@ -1,12 +1,13 @@
 package io.spring.hello_world;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
