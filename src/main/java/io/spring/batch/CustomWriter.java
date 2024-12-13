671a4b4015a99d0792663e50bf3dd33fdaaf1d1f
doxcom <aldoperez1319@gmail.com>
2024-12-12 23:18:38 -0600

    spring batch segundo intento

---
+++ src/main/java/io/spring/batch/CustomWriter.java
@@ -0,0 +1,18 @@
+package io.spring.batch;
+
+import org.springframework.batch.item.Chunk;
+import org.springframework.batch.item.ItemWriter;
+
+public class CustomWriter implements ItemWriter<String> {
+
+    @Override
+    public void write(Chunk<? extends String> chunk) throws Exception {
+
+        for (String data : chunk) {
+            System.out.println("Writing data: " + data);
+        }
+
+        System.out.println("completed writing data.");
+
+    }
+}
--- src/main/java/io/spring/configuration/JobConfiguration.java
+++ src/main/java/io/spring/configuration/JobConfiguration.java
@@ -1,48 +1,42 @@
 package io.spring.configuration;
 
+
+import io.spring.batch.CustomProcessor;
+import io.spring.batch.CustomReader;
+import io.spring.batch.CustomWriter;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
-
-import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @Configuration
-@EnableBatchProcessing
-@EnableTransactionManagement
-@EnableAutoConfiguration
 public class JobConfiguration {
 
-
+    @Bean
+    Job createJob(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new JobBuilder("job", jobRepository)
+                .flow(createStep(jobRepository,transactionManager)).end().build();
+    }
 
     @Bean
-    public Job job(JobRepository jobRepository, Step step) {
-        return new JobBuilder("step1", jobRepository)
-                .start(step)
+    Step createStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("step", jobRepository)
+                .<String, String> chunk(3,transactionManager)
+                .allowStartIfComplete(true)
+                .reader(new CustomReader())
+                .processor(new CustomProcessor())
+                .writer(new CustomWriter())
                 .build();
     }
 
 
-    @Bean
-    public Step step(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
-        return new StepBuilder("step", jobRepository)
-                .tasklet((StepContribution contribution,ChunkContext chunkContext) ->{
-                    System.out.println("hello world");
-                    return RepeatStatus.FINISHED;
-                },transactionManager).build();
-    }
 
    }
 
